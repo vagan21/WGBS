4113bf64ab2488d112ae47a589282d41e2f56e4f
Verda Agan <veax21@gmail.com>
2020-12-08 12:08:36 -0600

    moved WGBS scripts into WGBS folder

--- Cellspecific_Shared_HMRScript.sh
+++
@@ -1,8 +0,0 @@
-
-
-##### Cell specific 
-bedtools intersect -v -a CT_ofInterest -b Other_CT > CT\_all_cellspecific.txt
-
-
-##### ((1)) ALL_shared
-bedtools intersect -u -a CT_ofInterest -b $Other_CT > CT\_all_shared.txt
--- filtsizeandrefseq_Tim.sh
+++
@@ -1,8 +0,0 @@
-# Script to size filter, then cross ref vs RefSeq TSS/Exons
-# 2 steps: #1 filt size, #2 filt for refseq
-# usage: sh filtsizeandrefseq.sh [file] [size]
-size="$2"
-infile="$1"
-infileName=${infile%.*}
-outName=$infileName.minsize$size.filtforrefseqTSSexons.txt
-awk -v size="$size" 'BEGIN{OFS=FS="\t"}{if (($3-$2)>size) {print $1,$2,$3,($3-$2)}}' $infile | bedtools intersect -v -a - -b NCBI.RefSeq.TSS.Exons.merged.txt > $outName
